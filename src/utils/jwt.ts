cbdd59b6583e757f718f6ebb79cf5b70274f5272
Denning Kahky <35266252+denningscode@users.noreply.github.com>
2025-08-21 06:03:30 +0100

    Update jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,42 +1,10 @@
-/*import jwt, { SignOptions } from 'jsonwebtoken';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'fallback-secret-key';
-const JWT_EXPIRES_IN = (process.env.JWT_EXPIRES_IN || '7d') as string;
-
-export interface JWTPayload {
-  userId: string;
-  email: string;
-  username: string;
-}
-
-export const generateToken = (payload: JWTPayload): any => {
-  const options: SignOptions = { expiresIn: JWT_EXPIRES_IN };
-  return jwt.sign(payload, JWT_SECRET, options);
-};
-
-export const verifyToken = (token: string): JWTPayload => {
-  try {
-    const decoded = jwt.verify(token, JWT_SECRET);
-    return decoded as JWTPayload;
-  } catch (error) {
-    throw new Error('Invalid token');
-  }
-};
-
-export const decodeToken = (token: string): JWTPayload | null => {
-  try {
-    const decoded = jwt.decode(token);
-    return decoded as JWTPayload;
-  } catch (error) {
-    return null;
-  }
-};*/
 
 
 import jwt, { SignOptions } from "jsonwebtoken";
 
 
-const JWT_SECRET = process.env.JWT_SECRET || "lrtsdebugwiththissecret";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const JWT_EXPIRES_IN: SignOptions["expiresIn"] =
   (process.env.JWT_EXPIRES_IN as unknown as SignOptions["expiresIn"]) || "7d";
