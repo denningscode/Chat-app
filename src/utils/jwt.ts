11a9bed9fb5e6d9681e35a0d46dbe523425d546c
Denning Kahky <35266252+denningscode@users.noreply.github.com>
2025-08-21 05:55:28 +0100

    Update jwt.ts

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -39,7 +39,7 @@ if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET must be defined");
 }
 
-const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_SECRET = process.env.JWT_SECRET || "lrtsdebugwiththissecret";
 
 const JWT_EXPIRES_IN: SignOptions["expiresIn"] =
   (process.env.JWT_EXPIRES_IN as unknown as SignOptions["expiresIn"]) || "7d";
